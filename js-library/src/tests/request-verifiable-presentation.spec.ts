0b4f3cc7482e93bd753dc458154a133ffcd7407d
Llorenç <llorenc.muntaner@dfinity.org>
2024-04-23 11:27:24 +0200

    Fix test

--- js-library/src/tests/request-verifiable-presentation.spec.ts
+++ js-library/src/tests/request-verifiable-presentation.spec.ts
@@ -1,7 +1,7 @@
-import { requestVerifiableCredentials } from "..";
+import { requestVerifiablePresentation } from "../request-verifiable-presentation";
 
 describe("Request Verifiable Credentials function", () => {
   it("returns 2", () => {
-    expect(requestVerifiableCredentials()).toBe(2);
+    expect(requestVerifiablePresentation()).toBe(2);
   });
 });
