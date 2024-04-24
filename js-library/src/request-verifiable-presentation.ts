3dc38be681670b338b8350eb6ee077dbbe5f3aa8
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2024-04-24 12:01:17 +0200

    Add functionality and test the happy path (#3)
    # Motivation
    
    Create a first real test working before moving to more complex tests and before refactoring the code to make it look nice.
    
    I realized there were also issues with the spec files that didn't have prettier or lint working.
    
    # Changes
    
    * Add extra tsconfig.spec that includes test files.
    * Allow lint config to check test files.
    * Run `tsc` before running tests.
    * Add the jest types.
    * Add the code for `requestVerifiablePresentation`. DO NOT REVIEW THIS YET. This is working functionality but it will be refactored a lot.
    * Add a tsconfig.json within the test folder.
    
    # Tests
    
    * Add a test for the success functionality of `requestVerifiablePresentation`.

--- js-library/src/request-verifiable-presentation.ts
+++ js-library/src/request-verifiable-presentation.ts
@@ -1,9 +1,90 @@
-export type CredentialsSpec = {
-  type: string;
+export type CredentialData = {
+  credentialSpec: CredentialSpec;
+  credentialSubject: string;
+};
+
+export type CredentialSpec = {
+  credentialType: string;
   arguments: Record<string, string | number>;
 };
 
-export const requestVerifiablePresentation = () => {
-  console.log("Hello, world");
-  return 2;
+export type IssuerData = {
+  origin: string;
+  canisterId?: string;
+};
+
+let iiWindow: Window | null = null;
+let nextFlowId = 0;
+
+export const requestVerifiablePresentation = ({
+  onSuccess,
+  onError,
+  credentialData: { credentialSpec, credentialSubject },
+  issuerData,
+  windowOpenerFeatures,
+  derivationOrigin,
+  identityProvider,
+}: {
+  onSuccess: (verifiablePresentation: string) => void | Promise<void>;
+  onError: (err?: string) => void | Promise<void>;
+  credentialData: CredentialData;
+  issuerData: IssuerData;
+  windowOpenerFeatures?: string;
+  derivationOrigin: string | undefined;
+  identityProvider: string;
+}) => {
+  nextFlowId += 1;
+  const startFlow = (evnt: MessageEvent) => {
+    const req = {
+      id: String(nextFlowId),
+      jsonrpc: "2.0",
+      method: "request_credential",
+      params: {
+        issuer: issuerData,
+        credentialSpec,
+        credentialSubject,
+        derivationOrigin: derivationOrigin,
+      },
+    };
+    window.addEventListener("message", handleFlowFinished);
+    window.removeEventListener("message", handleFlowReady);
+    evnt.source?.postMessage(req, { targetOrigin: evnt.origin });
+  };
+  const finishFlow = async (evnt: MessageEvent) => {
+    try {
+      if (evnt.data?.error !== undefined) {
+        throw new Error(evnt.data.error);
+      }
+      // Make the presentation presentable
+      const verifiablePresentation = evnt.data?.result?.verifiablePresentation;
+      if (verifiablePresentation === undefined) {
+        // This should never happen
+        onError("Couldn't get the verifiable credential");
+      } else {
+        onSuccess(verifiablePresentation);
+      }
+    } catch (err) {
+      onError(`Error getting the verifiable credential: ${err}`);
+    } finally {
+      iiWindow?.close();
+      window.removeEventListener("message", handleFlowFinished);
+    }
+  };
+  const handleFlowFinished = (evnt: MessageEvent) => {
+    if (evnt.data?.method === "vc-flow-ready") {
+      startFlow(evnt);
+    } else if (evnt.data?.id === String(nextFlowId)) {
+      finishFlow(evnt);
+    }
+  };
+  const handleFlowReady = (evnt: MessageEvent) => {
+    if (evnt.data?.method !== "vc-flow-ready") {
+      return;
+    }
+    startFlow(evnt);
+  };
+  window.addEventListener("message", handleFlowReady);
+  const url = new URL(identityProvider);
+  url.pathname = "vc-flow/";
+  iiWindow = window.open(url, "_blank", windowOpenerFeatures);
 };
--- js-library/src/tests/request-verifiable-presentation.spec.ts
+++ js-library/src/tests/request-verifiable-presentation.spec.ts
@@ -1,7 +1,60 @@
+import { vi } from "vitest";
 import { requestVerifiablePresentation } from "../request-verifiable-presentation";
 
 describe("Request Verifiable Credentials function", () => {
-  it("returns 2", () => {
-    expect(requestVerifiablePresentation()).toBe(2);
+  const credentialSubject =
+    "2vtpp-r6lcd-cbfas-qbabv-wxrv5-lsrkj-c4dtb-6ets3-srlqe-xpuzf-vqe";
+  const identityProvider = "https://identity.ic0.app";
+  const issuerOrigin = "https://metaissuer.vc/";
+  // Source: https://github.com/dfinity/internet-identity/blob/6df217532c7e3d4d465decbd9159ceab5262ba2d/src/vc-api/src/index.ts#L9
+  const VcFlowReady = {
+    jsonrpc: "2.0",
+    method: "vc-flow-ready",
+  };
+  const verifiablePresentation = "12345";
+  const vcVerifiablePresentationMessage = {
+    // id should match the received id.
+    // We know it starts with 1 because nextFlowId is incremented before the request is sent.
+    // Ideally, we would record the one sent from the request and use it here.
+    // TODO: Record the id passed in the reply to the first call and use it here.
+    id: "1",
+    jsonrpc: "2.0",
+    result: {
+      verifiablePresentation,
+    },
+  };
+
+  beforeEach(() => {
+    window.open = vi.fn();
   });
+
+  it("calls onSuccess with a verifiable presentation", async () =>
+    new Promise<void>((done) => {
+      const onError = vi.fn();
+      requestVerifiablePresentation({
+        onSuccess: (presentation: string) => {
+          expect(presentation).toEqual(verifiablePresentation);
+          expect(onError).not.toHaveBeenCalled();
+          done();
+        },
+        onError,
+        credentialData: {
+          credentialSpec: {
+            credentialType: "MembershipCredential",
+            arguments: {
+              organization: "DFINITY",
+            },
+          },
+          credentialSubject,
+        },
+        issuerData: {
+          origin: issuerOrigin,
+        },
+        derivationOrigin: undefined,
+        identityProvider,
+      });
+      expect(window.open).toHaveBeenCalledTimes(1);
+      window.postMessage(VcFlowReady, "*");
+      window.postMessage(vcVerifiablePresentationMessage, "*");
+    }));
 });
