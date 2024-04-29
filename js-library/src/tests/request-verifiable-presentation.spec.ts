aab3ac5400f429a5535fc68696bd45ca694213cf
Llorenç <llorenc.muntaner@dfinity.org>
2024-04-29 13:00:39 +0200

    Use UUID for requests

--- js-library/src/tests/request-verifiable-presentation.spec.ts
+++ js-library/src/tests/request-verifiable-presentation.spec.ts
@@ -1,8 +1,8 @@
 import { vi } from "vitest";
 import {
   requestVerifiablePresentation,
-  resetNextFlowId,
   type CredentialRequestData,
+  type CredentialsRequest,
 } from "../request-verifiable-presentation";
 import { credentialPresentationMock } from "./mocks";
 
@@ -15,28 +15,22 @@ describe("Request Verifiable Credentials function", () => {
   const issuerData = {
     origin: issuerOrigin,
   };
-
   // Source: https://github.com/dfinity/internet-identity/blob/6df217532c7e3d4d465decbd9159ceab5262ba2d/src/vc-api/src/index.ts#L9
   const VcFlowReady = {
     jsonrpc: "2.0",
     method: "vc-flow-ready",
   };
-  const expectedFlowId = "1";
-  const vcVerifiablePresentationMessageSuccess = {
-    // id should match the received id.
-    // We know it starts with 1 because nextFlowId is incremented before the request is sent.
-    // Ideally, we would record the one sent from the request and use it here.
-    // TODO: Record the id passed in the reply to the first call and use it here.
-    id: expectedFlowId,
+  const vcVerifiablePresentationMessageSuccess = (id: string) => ({
+    id,
     jsonrpc: "2.0",
     result: {
       verifiablePresentation: credentialPresentationMock,
     },
-  };
-  const vcVerifiablePresentationMessageError = {
-    id: "1",
+  });
+  const vcVerifiablePresentationMessageError = (id: string) => ({
+    id,
     error: "Error getting the verifiable credential",
-  };
+  });
   const credentialData: CredentialRequestData = {
     credentialSpec: {
       credentialType: "MembershipCredential",
@@ -48,206 +42,229 @@ describe("Request Verifiable Credentials function", () => {
   };
 
   const credentialPresentationSuccess = credentialPresentationMock;
-
-  let sourcePostMessageSpy;
   const unreachableFn = () => {
     expect.unreachable("this function should not be called");
   };
 
   beforeEach(() => {
     window.open = vi.fn();
-    sourcePostMessageSpy = vi.fn();
-    resetNextFlowId();
   });
+
+  const startVcFlow = (): Promise<{
+    request: CredentialsRequest;
+    options: Record<string, string>;
+  }> => {
+    return new Promise((resolve) => {
+      window.dispatchEvent(
+        new MessageEvent("message", {
+          source: {
+            postMessage: (
+              request: CredentialsRequest,
+              options: Record<string, string>,
+            ) => {
+              resolve({ request, options });
+            },
+          } as Window,
+          origin: identityProvider,
+          data: {
+            jsonrpc: "2.0",
+            method: "vc-flow-ready",
+          },
+        }),
+      );
+    });
+  };
 
   const mockMessageFromIdentityProvider = (data: unknown) => {
     window.dispatchEvent(
       new MessageEvent("message", {
         source: {
-          postMessage: sourcePostMessageSpy,
-        } as Window,
+          postMessage: vi.fn(),
+        } as unknown as Window,
         origin: identityProvider,
         data,
       }),
     );
   };
 
-  it("opens new window and calls onSuccess with a verifiable presentation", async () =>
-    new Promise<void>((done) => {
-      requestVerifiablePresentation({
-        onSuccess: (presentation: string) => {
-          expect(presentation).toEqual(credentialPresentationSuccess);
-          done();
-        },
-        onError: unreachableFn,
-        credentialData,
-        issuerData,
-        derivationOrigin: undefined,
-        identityProvider,
-      });
-      expect(window.open).toHaveBeenCalledTimes(1);
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess);
-    }));
+  it("opens new window and calls onSuccess with a verifiable presentation", async () => {
+    const onSuccess = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess,
+      onError: unreachableFn,
+      credentialData,
+      issuerData,
+      derivationOrigin: undefined,
+      identityProvider,
+    });
 
-  it("calls onSuccess with a verifiable presentation", async () =>
-    new Promise<void>((done) => {
-      requestVerifiablePresentation({
-        onSuccess: (presentation: string) => {
-          expect(presentation).toEqual(credentialPresentationSuccess);
-          done();
-        },
-        onError: unreachableFn,
-        credentialData: {
-          credentialSpec: {
-            credentialType: "MembershipCredential",
-            arguments: {
-              organization: "DFINITY",
-            },
-          },
-          credentialSubject,
-        },
-        issuerData: {
-          origin: issuerOrigin,
-        },
-        derivationOrigin: undefined,
-        identityProvider,
-      });
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess);
-    }));
+    const {
+      request: { id },
+    } = await startVcFlow();
+    mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess(id));
 
-  it("send expected request to the identity provider with derivationOrigin", () =>
-    new Promise<void>((done) => {
-      requestVerifiablePresentation({
-        onSuccess: () => {
-          expect(sourcePostMessageSpy).toHaveBeenCalledTimes(1);
-          expect(sourcePostMessageSpy).toHaveBeenCalledWith(
-            {
-              id: expectedFlowId,
-              jsonrpc: "2.0",
-              method: "request_credential",
-              params: {
-                issuer: issuerData,
-                credentialSpec: credentialData.credentialSpec,
-                credentialSubject,
-                derivationOrigin,
-              },
-            },
-            {
-              targetOrigin: identityProvider,
-            },
-          );
-          done();
-        },
-        onError: unreachableFn,
-        credentialData,
-        issuerData,
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    expect(onSuccess).toBeCalledWith(credentialPresentationMock);
+    expect(window.open).toHaveBeenCalledTimes(1);
+  });
+
+  it("send expected request to the identity provider with derivationOrigin", async () => {
+    const onSuccess = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess,
+      onError: unreachableFn,
+      credentialData,
+      issuerData,
+      derivationOrigin,
+      identityProvider,
+    });
+
+    const { request, options } = await startVcFlow();
+    mockMessageFromIdentityProvider(
+      vcVerifiablePresentationMessageSuccess(request.id),
+    );
+
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    expect(request).toEqual({
+      id: expect.any(String),
+      jsonrpc: "2.0",
+      method: "request_credential",
+      params: {
+        issuer: issuerData,
+        credentialSpec: credentialData.credentialSpec,
+        credentialSubject,
         derivationOrigin,
-        identityProvider,
-      });
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess);
-    }));
+      },
+    });
+    expect(options).toEqual({ targetOrigin: identityProvider });
+  });
 
-  it("is successful with multiple flow-ready messages", async () =>
-    new Promise<void>((done) => {
-      const onError = vi.fn();
-      requestVerifiablePresentation({
-        onSuccess: (presentation: string) => {
-          expect(presentation).toEqual(credentialPresentationSuccess);
-          expect(onError).not.toHaveBeenCalled();
-          done();
-        },
-        onError: unreachableFn,
-        credentialData,
-        issuerData,
-        derivationOrigin: undefined,
-        identityProvider,
-      });
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess);
-    }));
+  it("is successful with multiple flow-ready messages", async () => {
+    const onSuccess = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess,
+      onError: unreachableFn,
+      credentialData,
+      issuerData,
+      derivationOrigin: undefined,
+      identityProvider,
+    });
+    const {
+      request: { id },
+    } = await startVcFlow();
+    mockMessageFromIdentityProvider(VcFlowReady);
+    mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess(id));
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+  });
 
-  it("waits until the expected id is received", () =>
-    new Promise<void>((done) => {
-      requestVerifiablePresentation({
-        onSuccess: (presentation: string) => {
-          expect(presentation).toEqual(credentialPresentationSuccess);
-          done();
-        },
-        onError: unreachableFn,
-        credentialData,
-        issuerData,
-        derivationOrigin: undefined,
-        identityProvider,
-      });
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider({
-        id: "not-expected-id",
-        ...vcVerifiablePresentationMessageSuccess,
-      });
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess);
-    }));
+  it("waits until the expected id is received", async () => {
+    const onSuccess = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess,
+      onError: unreachableFn,
+      credentialData,
+      issuerData,
+      derivationOrigin: undefined,
+      identityProvider,
+    });
+    const {
+      request: { id },
+    } = await startVcFlow();
+    mockMessageFromIdentityProvider(
+      vcVerifiablePresentationMessageSuccess("wrong-id"),
+    );
+    expect(onSuccess).not.toHaveBeenCalled();
+    mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess(id));
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+  });
 
-  it("ignores messages before starting the flow", () =>
-    new Promise<void>((done) => {
-      requestVerifiablePresentation({
-        onSuccess: (presentation: string) => {
-          expect(presentation).toEqual(credentialPresentationSuccess);
-          done();
-        },
-        onError: unreachableFn,
-        credentialData,
-        issuerData,
-        derivationOrigin: undefined,
-        identityProvider,
-      });
-      // This doesn't trigger an error because it's before the flow starts.
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageError);
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess);
-    }));
+  it("ignores messages before starting the flow", async () => {
+    const onSuccess = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess,
+      onError: unreachableFn,
+      credentialData,
+      issuerData,
+      derivationOrigin: undefined,
+      identityProvider,
+    });
+    mockMessageFromIdentityProvider({
+      id: "1",
+      error: "Error getting the verifiable credential",
+    });
+    expect(onSuccess).not.toHaveBeenCalled();
+    const {
+      request: { id },
+    } = await startVcFlow();
+    mockMessageFromIdentityProvider(vcVerifiablePresentationMessageSuccess(id));
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+  });
 
-  it("calls onError when the credential fails", () =>
-    new Promise<void>((done) => {
-      requestVerifiablePresentation({
-        onSuccess: unreachableFn,
-        onError: (err: unknown) => {
-          expect(err).toEqual(
-            `Error getting the verifiable credential: ${vcVerifiablePresentationMessageError.error}`,
-          );
-          done();
-        },
-        credentialData,
-        issuerData,
-        derivationOrigin: undefined,
-        identityProvider,
-      });
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(vcVerifiablePresentationMessageError);
-    }));
+  it("calls onError when the credential fails", async () => {
+    const onError = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess: unreachableFn,
+      onError,
+      credentialData,
+      issuerData,
+      derivationOrigin: undefined,
+      identityProvider,
+    });
+    const {
+      request: { id },
+    } = await startVcFlow();
+    mockMessageFromIdentityProvider(vcVerifiablePresentationMessageError(id));
+    expect(onError).toHaveBeenCalledTimes(1);
+    expect(onError).toHaveBeenCalledWith(
+      `Error getting the verifiable credential: ${vcVerifiablePresentationMessageError("id").error}`,
+    );
+  });
 
-  it("calls onError when there is no verifiable presentation", () =>
-    new Promise<void>((done) => {
-      const noCredential = { id: "1", jsonrpc: "2.0" };
-      requestVerifiablePresentation({
-        onSuccess: unreachableFn,
-        onError: (err: unknown) => {
-          expect(err).toEqual(
-            `Error getting the verifiable credential: Key 'verifiablePresentation' not found in the message data: ${JSON.stringify(noCredential)}`,
-          );
-          done();
-        },
-        credentialData,
-        issuerData,
-        derivationOrigin: undefined,
-        identityProvider,
-      });
-      mockMessageFromIdentityProvider(VcFlowReady);
-      mockMessageFromIdentityProvider(noCredential);
-    }));
+  it("calls onError when there is no verifiable presentation", async () => {
+    const onError = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess: unreachableFn,
+      onError,
+      credentialData,
+      issuerData,
+      derivationOrigin: undefined,
+      identityProvider,
+    });
+    const {
+      request: { id },
+    } = await startVcFlow();
+    const noCredential = { id, jsonrpc: "2.0" };
+    mockMessageFromIdentityProvider(noCredential);
+    expect(onError).toHaveBeenCalledTimes(1);
+    expect(onError).toHaveBeenCalledWith(
+      `Error getting the verifiable credential: Key 'verifiablePresentation' not found in the message data: ${JSON.stringify(noCredential)}`,
+    );
+  });
+
+  it("calls onError if decoding credential fails", async () => {
+    const onError = vi.fn();
+    requestVerifiablePresentation({
+      onSuccess: unreachableFn,
+      onError,
+      credentialData,
+      issuerData,
+      derivationOrigin: undefined,
+      identityProvider,
+    });
+    const {
+      request: { id },
+    } = await startVcFlow();
+    mockMessageFromIdentityProvider({
+      id,
+      result: {
+        verifiablePresentation: "invalid",
+      },
+    });
+    expect(onError).toHaveBeenCalledTimes(1);
+    expect(onError).toHaveBeenCalledWith(
+      "Error getting the verifiable credential: Decoding credentials failed: JWTInvalid: Invalid JWT",
+    );
+  });
 
   // TODO: Add functionality after refactor.
   it.skip("ignores messages from other origins than identity provider", () =>
