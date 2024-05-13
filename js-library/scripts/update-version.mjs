b1bda493ea1bf8b9962770d3d3a063b98f9c7068
Llorenç <llorenc.muntaner@dfinity.org>
2024-05-13 11:11:39 +0200

    Add reading from package.json

--- js-library/scripts/update-version.mjs
+++ js-library/scripts/update-version.mjs
@@ -32,13 +32,17 @@ const updateVersion = async () => {
     return;
   }
 
-  const { version: currentVersion, ...rest } = JSON.parse(
-    readFileSync(packagePath, "utf-8"),
-  );
+  const {
+    version: currentVersion,
+    name,
+    ...rest
+  } = JSON.parse(readFileSync(packagePath, "utf-8"));
+
+  const project = name.replace("@dfinity/", "");
 
   // Build wip version number
   const version = await nextVersion({
-    project: "verifiable-credentials",
+    project,
     currentVersion,
   });
 
