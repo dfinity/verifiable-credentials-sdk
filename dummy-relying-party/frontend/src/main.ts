b41d8f2a6838ad186ffbe073bd372af770dc6592
Llorenç <llorenc.muntaner@dfinity.org>
2024-05-28 15:03:34 +0200

    Configure all in FE instead of env vars

--- dummy-relying-party/frontend/src/main.ts
+++ dummy-relying-party/frontend/src/main.ts
@@ -4,55 +4,136 @@ import {
   requestVerifiablePresentation,
   type VerifiablePresentationResponse,
 } from "@dfinity/verifiable-credentials/request-verifiable-presentation";
+import { Principal } from "@dfinity/principal";
 
-const II_URL = import.meta.env.VITE_INTERNET_IDENTITY_URL;
-const ISSUER_ORIGIN = import.meta.env.VITE_ISSUER_ORIGIN;
-const ISSUER_CANISTER_ID = import.meta.env.VITE_ISSUER_CANISTER_ID;
-const loginButton = document.getElementById("login");
 const vcContainer = document.getElementById("vc-container");
-const earlyAdopterVCButton = document.getElementById("start-early-adopter-vc");
-const attendeeVCForm =
-  (document.getElementById("request-attendee-credential") as HTMLFormElement) ||
-  null;
-const loginStatus = document.getElementById("login-status");
+const loggedInContainer = document.getElementById("logged-in");
+const hideLoggedIn = () => {
+  loggedInContainer?.classList.add("hidden");
+  vcContainer?.classList.add("hidden");
+};
+
+const showLoggedIn = () => {
+  loggedInContainer?.classList.remove("hidden");
+};
+
+const showVcContainer = () => {
+  vcContainer?.classList.remove("hidden");
+};
+
+const hideNotLoggedIn = () => {
+  document.getElementById("not-logged-in")?.classList.add("hidden");
+};
+
+let iiURL: string | null = null;
+
+hideLoggedIn();
+
+const loginButton = document.getElementById("login");
+const requesetCredentialForm =
+  (document.getElementById("request-credential") as HTMLFormElement) || null;
+const userPrincipal = document.getElementById("user-principal");
+const addArgumentButton = document.getElementById("add-argument");
 const authClient = await AuthClient.create();
 loginButton?.addEventListener("click", async () => {
-  await authClient.login({
-    identityProvider: II_URL,
-    onSuccess: () => {
-      loginButton?.classList.add("hidden");
-      vcContainer?.classList.remove("hidden");
-      if (loginStatus) {
-        loginStatus.innerText = `Logged in as ${authClient
-          .getIdentity()
-          .getPrincipal()
-          .toText()}`;
+  const iiURLElement = document.getElementById(
+    "ii-url"
+  ) as HTMLInputElement | null;
+  if (iiURLElement) {
+    iiURL = iiURLElement.value;
+    await authClient.login({
+      identityProvider: iiURL,
+      onSuccess: () => {
+        showLoggedIn();
+        hideNotLoggedIn();
+        if (userPrincipal) {
+          userPrincipal.innerText = `User principal: ${authClient
+            .getIdentity()
+            .getPrincipal()
+            .toText()}`;
+        }
+      },
+    });
+  } else {
+    alert("Please provide an Identity Provider URL");
+  }
+});
+
+const credentialArguments: Record<string, string | number> = {};
+
+addArgumentButton?.addEventListener("click", () => {
+  const argumentsContainer = document.getElementById("arguments");
+  if (argumentsContainer) {
+    const key = document.getElementById("argument-key") as HTMLInputElement;
+    const value = document.getElementById("argument-value") as HTMLInputElement;
+    const argType = document.getElementById(
+      "argument-type"
+    ) as HTMLSelectElement;
+    if (key && value && argType) {
+      credentialArguments[key.value] =
+        argType.value === "number" ? Number(value.value) : value.value;
+      key.value = "";
+      value.value = "";
+    }
+    argumentsContainer.innerHTML = "";
+    const argumentTemplate = document.getElementById("arguments-template");
+    Object.entries(credentialArguments).forEach(([key, value]) => {
+      if (argumentTemplate) {
+        const newArgument = argumentTemplate.cloneNode(true) as HTMLElement;
+        newArgument.id = "";
+        const keyElement = newArgument.querySelector(".key");
+        const valueElement = newArgument.querySelector(".value");
+        const typeElement = newArgument.querySelector(".type");
+        if (keyElement && valueElement && typeElement) {
+          keyElement.innerHTML = `${key}: `;
+          valueElement.innerHTML = `${String(value)} - `;
+          typeElement.innerHTML = `type: ${typeof value}`;
+        }
+        newArgument.classList.remove("hidden");
+        argumentsContainer.appendChild(newArgument);
       }
-    },
+    });
+  }
+});
+
+requesetCredentialForm?.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(requesetCredentialForm);
+  const issuerUrl = formData.get("issuer-url") as string;
+  const credentialType = formData.get("credential-type") as string;
+  requestCredentials({
+    issuerUrl,
+    credentialType,
+    credentialArgs: credentialArguments,
   });
 });
 
-earlyAdopterVCButton?.addEventListener("click", async () => {
-  requestCredentials("EarlyAdopter", { sinceYear: 2024 });
-});
-
-attendeeVCForm?.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const formData = new FormData(attendeeVCForm);
-  const eventName = formData.get("event-name") as string;
-  requestCredentials("EventAttendance", { eventName });
-});
-
-const requestCredentials = async (
-  credentialType: string,
-  credentialArgs: Record<string, string | number>
-) => {
+const requestCredentials = async ({
+  issuerUrl,
+  canisterId,
+  derivationOrigin,
+  credentialType,
+  credentialArgs,
+}: {
+  issuerUrl: string;
+  canisterId?: Principal;
+  derivationOrigin?: string;
+  credentialType: string;
+  credentialArgs: Record<string, string | number>;
+}) => {
+  // We shouldn't happen because we don't show the form to request credentials
+  // until the user is logged in.
+  if (!iiURL) {
+    alert("Please login first");
+    return;
+  }
   const identity = authClient.getIdentity();
   const principal = identity.getPrincipal().toText();
   requestVerifiablePresentation({
     onSuccess: async (
       verifiablePresentation: VerifiablePresentationResponse
     ) => {
+      showVcContainer();
       const resultElement = document.getElementById("vc-result");
       const presentationElement = document.getElementById("vc-presentation");
       const aliasElement = document.getElementById("vc-alias");
@@ -89,8 +170,8 @@ const requestCredentials = async (
       }
     },
     issuerData: {
-      origin: ISSUER_ORIGIN,
-      canisterId: ISSUER_CANISTER_ID,
+      origin: issuerUrl,
+      canisterId: canisterId?.toText(),
     },
     credentialData: {
       credentialSpec: {
@@ -99,7 +180,7 @@ const requestCredentials = async (
       },
       credentialSubject: principal,
     },
-    identityProvider: II_URL,
-    derivationOrigin: undefined,
+    identityProvider: iiURL,
+    derivationOrigin,
   });
 };
