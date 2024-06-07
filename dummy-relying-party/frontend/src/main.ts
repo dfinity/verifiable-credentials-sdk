6652858b71fee4eee6c8e739e12a989a713fadca
Llorenç <llorenc.muntaner@dfinity.org>
2024-06-07 14:13:58 +0200

    Working nice UI

--- dummy-relying-party/frontend/src/main.ts
+++ dummy-relying-party/frontend/src/main.ts
@@ -32,9 +32,6 @@ const showCredentials = () => {
   requestCredentialForm?.classList.add("grid");
 };
 
-// showCredentials();
-showVcContainer();
-
 let iiURL: string | null = null;
 
 const userPrincipal = document.getElementById("user-principal");
@@ -118,6 +115,93 @@ requestCredentialButton?.addEventListener("click", async () => {
   });
 });
 
+const renderDecodedCredential = (jwt: string) => {
+  const container = document.getElementById("vc-credential-container");
+  const element = document.getElementById("vc-credential");
+  if (container && element) {
+    container.classList.remove("hidden");
+    container.classList.add("flex");
+    element.innerText = JSON.stringify(decodeJwt(jwt), null, 2);
+    window.scrollTo({ top: container.offsetTop, behavior: "smooth" });
+  }
+};
+
+const renderDecodedAlias = (jwt: string) => {
+  const aliasContainer = document.getElementById("vc-alias-container");
+  const aliasElement = document.getElementById("vc-alias");
+  if (aliasContainer && aliasElement) {
+    aliasContainer.classList.remove("hidden");
+    aliasContainer.classList.add("flex");
+    aliasElement.innerText = JSON.stringify(decodeJwt(jwt), null, 2);
+    window.scrollTo({ top: aliasContainer.offsetTop, behavior: "smooth" });
+  }
+};
+
+const renderDecodedCredentialPresentation = (jwt: string) => {
+  const presentationElement = document.getElementById("vc-presentation");
+  const presentationContainer = document.getElementById(
+    "vc-presentation-container"
+  );
+  if (presentationElement && presentationContainer) {
+    const decodedPresentation = decodeJwt(jwt) as any;
+    presentationContainer.classList.remove("hidden");
+    presentationContainer.classList.add("flex");
+    presentationElement.innerText = JSON.stringify(
+      decodedPresentation,
+      null,
+      2
+    );
+    window.scrollTo({
+      top: presentationContainer.offsetTop,
+      behavior: "smooth",
+    });
+
+    const [alias, credential] = decodedPresentation.vp.verifiableCredential;
+    document
+      .getElementById("decode-alias-button")
+      ?.addEventListener("click", () => {
+        renderDecodedAlias(alias);
+      });
+
+    document
+      .getElementById("decode-credential-button")
+      ?.addEventListener("click", () => {
+        renderDecodedCredential(credential);
+      });
+
+    document
+      .getElementById("copy-alias-button")
+      ?.addEventListener("click", () => {
+        navigator.clipboard.writeText(alias);
+      });
+
+    document
+      .getElementById("copy-credential-button")
+      ?.addEventListener("click", () => {
+        navigator.clipboard.writeText(credential);
+      });
+  }
+};
+
+const renderCredential = (jwt: string) => {
+  showVcContainer();
+  const resultElement = document.getElementById("vc-result");
+  if (resultElement) {
+    resultElement.innerText = jwt;
+  }
+  document
+    .getElementById("decode-credential-presentation-button")
+    ?.addEventListener("click", () => {
+      renderDecodedCredentialPresentation(jwt);
+    });
+
+  document
+    .getElementById("copy-credential-presentation-button")
+    ?.addEventListener("click", () => {
+      navigator.clipboard.writeText(jwt);
+    });
+};
+
 const requestCredentials = async ({
   issuerUrl,
   canisterId,
@@ -141,31 +225,12 @@ const requestCredentials = async ({
     onSuccess: async (
       verifiablePresentation: VerifiablePresentationResponse
     ) => {
-      showVcContainer();
       console.log("in onSuccess", verifiablePresentation);
       const resultElement = document.getElementById("vc-result");
-      const presentationElement = document.getElementById("vc-presentation");
-      const aliasElement = document.getElementById("vc-alias");
-      const credentialElement = document.getElementById("vc-credential");
       if ("Ok" in verifiablePresentation) {
-        if (resultElement) {
-          resultElement.innerText = verifiablePresentation.Ok;
-        }
-        const ver = decodeJwt(verifiablePresentation.Ok) as any;
-        if (presentationElement) {
-          presentationElement.innerText = JSON.stringify(ver, null, 2);
-        }
-        const creds = ver.vp.verifiableCredential;
-        const [alias, credential] = creds.map((cred: string) =>
-          JSON.stringify(decodeJwt(cred), null, 2)
-        );
-        if (aliasElement) {
-          aliasElement.innerText = alias;
-        }
-        if (credentialElement) {
-          credentialElement.innerText = credential;
-        }
+        renderCredential(verifiablePresentation.Ok);
       } else {
+        showVcContainer();
         if (resultElement) {
           resultElement.innerText = "Credential not obtained";
         }
@@ -187,7 +252,7 @@ const requestCredentials = async ({
       credentialSpec,
       credentialSubject: principal,
     },
-    identityProvider: iiURL,
+    identityProvider: new URL(iiURL).origin,
     derivationOrigin,
   });
 };
